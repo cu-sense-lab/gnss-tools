4c5d51b1c2fd0123428ce74bce814fa6e09fe98e
Brian Breitsch <brianbreitsch@gmail.com>
2025-03-17 22:41:18 -0600

    new pierce point code

--- gnss_tools/coords/__init__.py
+++ gnss_tools/coords/__init__.py
@@ -8,6 +8,9 @@ from .utils import (
     xyz2sky,
     sky2enu,
     ecf2sky,
+    meridional_radius_of_curvature,
+    normal_radius_of_curvature,
+    radius_of_curvature,
     local_radius_of_curvature,
     geo2ecf,
     geo2sky,
@@ -16,3 +19,5 @@ from .utils import (
     rotate_pos_ecf,
 )
 from .sun_coordinates import compute_sun_eci_coordinates
+
+from .pierce_point import compute_earth_pierce_point
